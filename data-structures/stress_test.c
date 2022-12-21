a6359e8a7bf26a3ec691f26bb751ee0576b107df
Ayush Tiwari <79035393+teltorob@users.noreply.github.com>
2022-12-21 23:53:33 +0530

    Update stress_test.c

--- data-structures/stress_test.c
+++ data-structures/stress_test.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // Stress test for loop till it fails
+    // Stress test for loop till it fails as it keeps increasing the float value
     for (float x = 0.000000; x < 0.000001; x = x + 0.0000001)
     {
         printf("Loop Stress Test: %f\n", x);
